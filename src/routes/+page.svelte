dcd8de69fcf2921accf78c2797b1a07fd17c43a7
Parjanya Brahmachari <pbrahmac@gmail.com>
2024-11-20 02:01:41 -0800

    Change header text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
 
 <div class="p-4 mt-20 flex flex-col items-center text-foreground/90">
   <div class="w-full max-w-screen-md py-4 flex flex-col items-center space-y-8">
-    <h1 class="font-medium text-xl">Your minimal currency converter.</h1>
+    <h1 class="font-medium text-xl">A minimal, offline currency converter.</h1>
     <Converter />
   </div>
 </div>
